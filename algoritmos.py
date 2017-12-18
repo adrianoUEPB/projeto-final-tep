791df7e8ac4342b066f9f1cfaa104cf60676f450
AdrianoAraujo <adriano.music.cg@gmail.com>
2017-12-18 12:56:47 -0300

    Organizando dados

--- algoritmos.py
+++ algoritmos.py
@@ -7,3 +7,20 @@ def mse(listTest, listPred):
         somatorio += x
 
     return math.sqrt(somatorio/listPred.__len__())
+
+def vizinhoMaisProximo(usuario1, usuario2):
+    soma1 = 0
+    for i in range(0, usuario1.__len__()):
+        soma1 += usuario1[i] * usuario2[i]
+
+    norma1 = 0
+    norma2 = 0
+    for i in usuario1:
+        norma1 += math.pow(i, 2)
+
+    for i in usuario2:
+        norma2 += math.pow(i, 2)
+
+    return soma1 / (math.sqrt(norma1) * math.sqrt(norma2))
+
+
--- colaborativo.py
+++ colaborativo.py
@@ -1,2 +1,34 @@
 import algoritmos as alg
+import numpy as np
 
+arquivo = open("teste.base", "r")
+# arquivo = open("dadosParaTEP/u1.base", "r")
+read = []
+for i in arquivo:
+    read.append(i.split("\t"))
+
+#criando a matriz de 0
+matriz = np.zeros((943, 1682), dtype=np.int)
+for i in range(0, read.__len__()):
+    matriz[int(read[i][0])-1][int(read[i][1])-1] = int(read[i][2])
+
+listaProximidade = []
+proximidade = {}
+for linha in range(0, 89):
+    for linha2 in range(0, 89):
+        # aux = linha
+        # while(aux-1 >= 0):
+        #     aux -= 1
+        #     dicionarioAuux =
+        #     if(listaProximidade[aux].__contains__('u{0}'.format(linha2+1))):
+        #         proximidade['u{0}'.format(linha2+1)] = listaProximidade[aux]['u{0}'.format(linha2+1)]
+        proximidade['u{0}'.format(linha2+1)] = alg.vizinhoMaisProximo(matriz[linha], matriz[linha2])
+
+    listaProximidade.append(proximidade)
+    proximidade = {}
+
+for i in listaProximidade:
+    print(i)
+
+# print(matriz)
+print('finalizado')
